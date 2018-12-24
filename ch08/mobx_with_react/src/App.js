05c9d26c6edf99261fc9a967cb9b3f167dc05de6
stepanow <stepanowon@gmail.com>
2018-12-24 19:50:31 +0900

    sss

---
+++ ch08/mobx_with_react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default App;
