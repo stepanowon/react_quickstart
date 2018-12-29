ef02a65305020002bad5a620e0f8a56a4d268646
stepanowon <stepanowon@hotmail.com>
2018-12-29 11:45:22 +0900

    ss

--- ch08/todolistapp_mobx/src/index.js
+++ ch08/todolistapp_mobx/src/index.js
@@ -10,10 +10,7 @@ import { Provider } from 'mobx-react';
 ReactDOM.render(
   <Provider todoStore={todoStore}>
     <App />
-  </Provider>,
-    document.getElementById('root')
-  );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+  </Provider>, document.getElementById('root')
+);
+
 serviceWorker.unregister();
