a9ebc514baf42cbbd844d94d6d55b8708273146f
stepanowon <stepanowon@hotmail.com>
2020-02-24 22:39:15 +0900

    webpacktest5

---
+++ ch03/webpacktest5/webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = { 
+    devtool : 'source-map',
+    mode : 'development',
+    entry: __dirname + '/src/main.js', 
+    output: { 
+        path: __dirname + '/public', 
+        filename: 'bundle.js' 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" }
+                ]
+            }        
+        ]
+    },
+    devServer : {
+        contentBase : './public',
+        inline:true, 
+        historyApiFallback :true,
+        port : 7777
+    }
+ };
+ 
---
+++ ch03/webpacktest5/webpack.prod.config.js
@@ -0,0 +1,31 @@
+module.exports = { 
+    mode : 'production',
+    entry: __dirname + '/src/main.js', 
+    output: { 
+        path: __dirname + '/public', 
+        filename: 'bundle.js' 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" }
+                ]
+            }  
+        ]
+    }
+};
+ 
