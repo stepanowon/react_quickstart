86d15bb28fe4622c85a8490214c45eca76cc4b33
stepanowon <stepanowon@hotmail.com>
2020-02-24 23:41:24 +0900

    webpacktest8

---
+++ ch03/webpacktest8/webpack.prod.config.js
@@ -0,0 +1,54 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = { 
+    mode : 'production',
+    entry: { 
+        main: __dirname + '/src/main.js' 
+    }, 
+    output: { 
+        path: __dirname + '/public', 
+        filename: '[name]-[hash].js' 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude : /\.module\.css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader  },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.module\.css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: "css-loader", options : { modules: true } }
+                ]
+            }  
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            title : '직원 정보 조회', 
+            template : __dirname + '/assets/index.html', 
+            filename : 'index.html'
+       }),
+       new MiniCssExtractPlugin({
+            filename: '[name]-[hash].css'
+       })
+    ]
+};
+ 
